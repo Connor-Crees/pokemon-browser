2e723d37ba779cfb6d40560aa5074ebf0cda58fc
Connor Crees <cic41@bath.ac.uk>
2025-05-17 14:15:50 +0100

    Search works with exact pokemon name

--- components/pokemon-list.tsx
+++ components/pokemon-list.tsx
@@ -4,27 +4,37 @@ import { Pokemon } from "@/lib/pokemonInterfaces";
 
 export async function PokemonList({ pageNum, search }: { pageNum: number, search: string }) {
     const pokemonCardList = [] as Pokemon[];
-    const pokemonList = await GetPokemonList(pageNum);
-    for(const pokemon of pokemonList){
-        const results = await GetPokemonCardDetails(pokemon.name);
-        const details : Pokemon = {name: results[0], id: results[1], types:results[2], image:results[3]}
-        pokemonCardList.push(details);
-    }
-    return (
-        <div>
-            <div className="flex justify-center flex-wrap gap-8">
-                {pokemonCardList.map((pokemon: Pokemon) => {
-                    return (
-                        <PokemonCard 
-                            name={pokemon.name} 
-                            id={pokemon.id} 
-                            types={pokemon.types} 
-                            image={pokemon.image} 
-                            key={pokemon.name} 
-                         />
-                    )
-                })}
+    try{
+        if(search != ""){
+            const results = await GetPokemonCardDetails(search);
+            const details : Pokemon = {name: results[0], id: results[1], types:results[2], image:results[3]}
+            pokemonCardList.push(details);
+        } else {
+            const pokemonList = await GetPokemonList(pageNum);
+            for(const pokemon of pokemonList){
+                const results = await GetPokemonCardDetails(pokemon.name);
+                const details : Pokemon = {name: results[0], id: results[1], types:results[2], image:results[3]}
+                pokemonCardList.push(details);
+            }
+        }
+        return (
+            <div>
+                <div className="flex justify-center flex-wrap gap-8">
+                    {pokemonCardList.map((pokemon: Pokemon) => {
+                        return (
+                            <PokemonCard 
+                                name={pokemon.name} 
+                                id={pokemon.id} 
+                                types={pokemon.types} 
+                                image={pokemon.image} 
+                                key={pokemon.name} 
+                            />
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    )
+        )
+    } catch{
+        return (<div>No Pokemon Found</div>)
+    }
 }
--- components/pokemon-search.tsx
+++ components/pokemon-search.tsx
@@ -5,28 +5,36 @@ import { Button } from '@/components/ui/button';
 import { usePathname, useSearchParams} from "next/navigation";
 import { useState } from 'react';
 import Link from 'next/link';
+import { parseParameter } from 'next/dist/shared/lib/router/utils/route-regex';
 
 export function PokemonSearch() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
     const [searchValue, setSearchValue] = useState("");
+    var exploreText = "Explore Pokémon";
+    const params = new URLSearchParams(searchParams);
+    const search = params.get("search");
+    if(search){
+        exploreText = `Search Results for '${search.charAt(0).toUpperCase() + search.slice(1)}'`
+    }
+    
 
     const createPageURL = (searchValue: string) => {
         const params = new URLSearchParams(searchParams);
-        params.set("search", searchValue.toString());
+        params.set("search", searchValue.toLowerCase());
         return(`${pathname}?${params.toString()}`);
     }
 
     return (
         <div className="flex justify-between">
-            <h2 className="flex items-center text-[30px] tracking-[-0.025em]">Explore Pokémon</h2>
+            <h2 className="flex items-center text-[30px] tracking-[-0.025em]">{exploreText}</h2>
             <div className='flex justify-right gap-4'>
                 <Input 
                     type = "search"
                     placeholder = 'Find Pokémon'
                     value = {searchValue}
-                    onChange = {e => {setSearchValue(e.currentTarget.value.toLowerCase());}}
+                    onChange = {e => {setSearchValue(e.currentTarget.value);}}
                 />
                 <Link href={createPageURL(searchValue)}>
                     <Button type = "submit">Search</Button>
